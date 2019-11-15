1b215c7ce8887ae0727b193a9f789f9fbf4e6844
Marvin <marvincollins14@gmail.com>
2019-11-15 17:35:57 +0300

    this is it man

--- src/RuhusaServiceProvider.php
+++ src/RuhusaServiceProvider.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace AppsLab\Acl;
 
 use AppsLab\Acl\Commands\CreatePermission;
@@ -22,7 +21,7 @@ class RuhusaServiceProvider extends ServiceProvider
         $this->app['router']->aliasMiddleware('roles', RoleMiddleware::class);
         $this->app['router']->aliasMiddleware('permissions', PermissionMiddleware::class);
 
-        if ($this->app->runningInConsole()){
+        if ($this->app->runningInConsole()) {
             $this->registerPublishing();
         }
         $this->registerResources();
@@ -33,16 +32,16 @@ class RuhusaServiceProvider extends ServiceProvider
 
     public function loadViews()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'ruhusa');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'ruhusa');
         $this->publishes([
-            __DIR__.'/../resources/views/layouts/app.blade.php' => resource_path('views/vendor/ruhusa/layouts/app.blade.php'),
-            __DIR__.'/../resources/views/acl/permission.blade.php' => resource_path('views/vendor/ruhusa/acl/permission.blade.php'),
-            __DIR__.'/../resources/views/acl/role.blade.php' => resource_path('views/vendor/ruhusa/acl/role.blade.php'),
-            __DIR__.'/../resources/views/acl/role-form-body.blade.php' => resource_path('views/vendor/ruhusa/acl/role-form-body.blade.php'),
-            __DIR__.'/../resources/views/acl/permission-form-body.blade.php' => resource_path('views/vendor/ruhusa/acl/permission-form-body.blade.php'),
-            __DIR__.'/../resources/views/acl/partials/_role-form.blade.php' => resource_path('views/vendor/ruhusa/acl/partials/_role-form.blade.php'),
-            __DIR__.'/../resources/views/acl/partials/_permission-form.blade.php' => resource_path('views/vendor/ruhusa/acl/partials/_permission-form.blade.php'),
-        ],'ruhusa-views');
+            __DIR__ . '/../resources/views/layouts/app.blade.php' => resource_path('views/vendor/ruhusa/layouts/app.blade.php'),
+            __DIR__ . '/../resources/views/acl/permission.blade.php' => resource_path('views/vendor/ruhusa/acl/permission.blade.php'),
+            __DIR__ . '/../resources/views/acl/role.blade.php' => resource_path('views/vendor/ruhusa/acl/role.blade.php'),
+            __DIR__ . '/../resources/views/acl/role-form-body.blade.php' => resource_path('views/vendor/ruhusa/acl/role-form-body.blade.php'),
+            __DIR__ . '/../resources/views/acl/permission-form-body.blade.php' => resource_path('views/vendor/ruhusa/acl/permission-form-body.blade.php'),
+            __DIR__ . '/../resources/views/acl/partials/_role-form.blade.php' => resource_path('views/vendor/ruhusa/acl/partials/_role-form.blade.php'),
+            __DIR__ . '/../resources/views/acl/partials/_permission-form.blade.php' => resource_path('views/vendor/ruhusa/acl/partials/_permission-form.blade.php'),
+        ], 'ruhusa-views');
     }
 
     public function register()
@@ -53,13 +52,12 @@ class RuhusaServiceProvider extends ServiceProvider
     private function registerResources()
     {
         $this->registerRoutes();
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
     public function registerRoutes()
     {
-        Route::group($this->routeConfig(), function (){
-            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        Route::group($this->routeConfig(), function () {
+            $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         });
     }
 
@@ -73,16 +71,16 @@ class RuhusaServiceProvider extends ServiceProvider
 
     private function registerBladeExtensions()
     {
-        Blade::directive('role', function ($role){
+        Blade::directive('role', function ($role) {
             return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) : ?>";
         });
 
-        Blade::directive('elserole', function ($role){
+        Blade::directive('elserole', function ($role) {
             return "<?php else if(auth()->check() && auth()->user()->hasRole({$role})) : ?>";
         });
 
-        Blade::directive('endrole', function (){
-            return "<?php endif; ?>";
+        Blade::directive('endrole', function () {
+            return '<?php endif; ?>';
         });
     }
 
@@ -90,26 +88,20 @@ class RuhusaServiceProvider extends ServiceProvider
     {
         //this is to allow you to modify the tables according to your project need
         $this->publishes([
-            __DIR__.'/../database/migrations/2018_10_12_000000_create_permissions_table.php' =>
-                'database/migrations/2018_10_12_000000_create_permissions_table.php',
-            __DIR__.'/../database/migrations/2018_10_12_000000_create_roles_table.php' =>
-                'database/migrations/2018_10_12_000000_create_roles_table.php',
-            __DIR__.'/../database/migrations/2018_11_24_105604_create_users_permissions_table.php' =>
-                'database/migrations/2018_11_24_105604_create_users_permissions_table.php',
-            __DIR__.'/../database/migrations/2018_11_24_105604_create_users_roles_table.php' =>
-                'database/migrations/2018_11_24_105604_create_users_roles_table.php',
-            __DIR__.'/../database/migrations/2018_11_24_110643_create_roles_permissions_table.php' =>
-                'database/migrations/2018_11_24_110643_create_roles_permissions_table.php',
+            __DIR__ . '/../database/migrations/2018_10_12_000000_create_permissions_table.php' => 'database/migrations/2018_10_12_000000_create_permissions_table.php',
+            __DIR__ . '/../database/migrations/2018_10_12_000000_create_roles_table.php' => 'database/migrations/2018_10_12_000000_create_roles_table.php',
+            __DIR__ . '/../database/migrations/2018_11_24_105604_create_users_permissions_table.php' => 'database/migrations/2018_11_24_105604_create_users_permissions_table.php',
+            __DIR__ . '/../database/migrations/2018_11_24_105604_create_users_roles_table.php' => 'database/migrations/2018_11_24_105604_create_users_roles_table.php',
+            __DIR__ . '/../database/migrations/2018_11_24_110643_create_roles_permissions_table.php' => 'database/migrations/2018_11_24_110643_create_roles_permissions_table.php',
             __DIR__ . '/../config/ruhusa.php' => 'config/ruhusa.php'
-
         ], 'ruhusa');
     }
 
     protected function authorize()
     {
-        if (config('ruhusa.models.permission') && Schema::hasTable(config('ruhusa.tables.permission'))){
-            app(config('ruhusa.models.permission'))::get()->map(function ($permission){
-                Gate::define($permission->slug, function ($user) use($permission){
+        if (config('ruhusa.models.permission') && Schema::hasTable(config('ruhusa.tables.permission'))) {
+            app(config('ruhusa.models.permission'))::get()->map(function ($permission) {
+                Gate::define($permission->slug, function ($user) use ($permission) {
                     return $user->hasPermissionTo($permission);
                 });
             });
@@ -118,7 +110,7 @@ class RuhusaServiceProvider extends ServiceProvider
 
     protected function loadCommands()
     {
-        if ($this->app->runningInConsole()){
+        if ($this->app->runningInConsole()) {
             $this->commands([
                 InstallPackage::class,
                 CreatePermission::class,
